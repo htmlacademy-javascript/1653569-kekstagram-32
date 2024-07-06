5101c577064ab3cb4e634ce6b20bd46c9f0fb96a
Reez74 <reez74@gmail.com>
2024-07-06 14:14:30 +0300

    Adds division into modules

---
+++ js/util.js
@@ -0,0 +1,27 @@
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  return Math.floor(Math.random() * (upper - lower + 1) + lower);
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+const createNumberGenerator = (min, max, {isUniqueId = false} = {}) => {
+  const prevValues = [];
+
+  return () => {
+    let currentValue = getRandomInteger(min, max);
+    if (isUniqueId) {
+      if (prevValues.length >= (max - min + 1)) {
+        return;
+      }
+      while (prevValues.includes(currentValue)) {
+        currentValue = getRandomInteger(min, max);
+      }
+      prevValues.push(currentValue);
+    }
+    return currentValue;
+  };
+};
+
+export {getRandomArrayElement, createNumberGenerator};

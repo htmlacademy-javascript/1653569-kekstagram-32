0112f0a88a324cf3baa128642647f57fd3b11561
Keks <keks@htmlacademy.ru>
2024-08-05 00:06:02 +0300

    :heavy_check_mark: Сборка #13

--- 13/js/image.js
+++ 13/js/image.js
@@ -1,10 +1,11 @@
-import { ClassName, toggleClass } from './utils.js';
+import { ClassName } from './utils.js';
 
 const MIN_SCALE = 25;
 const MAX_SCALE = 100;
 const SCALE_STEP = 25;
 const SCALE_DEFAULT = 100;
 const FILE_TYPES = ['jpg', 'jpeg', 'png'];
+const PREVIEW_DEFAULT = 'img/upload-default-image.jpg';
 
 const Effect = {
   CHROME: 'effect-chrome',
@@ -33,6 +34,15 @@ const Option = {
   [Effect.DEFAULT]: [0, 100, 1],
 };
 
+const Dependence = {
+  [Effect.CHROME]: {style: Filter.GRAYSCALE, unit: ''},
+  [Effect.SEPIA]: {style: Filter.SEPIA, unit: ''},
+  [Effect.MARVIN]: {style: Filter.INVERT, unit: '%'},
+  [Effect.FOBOS]: {style: Filter.BLUR, unit: 'px'},
+  [Effect.HEAT]: {style: Filter.BRIGHTNESS, unit: ''},
+  [Effect.DEFAULT]: {style: Filter.DEFAULT, unit: ''},
+};
+
 const modalContainer = document.querySelector('.img-upload');
 const imagePreview = modalContainer.querySelector('.img-upload__preview > img');
 const imageScale = modalContainer.querySelector('.scale__control--value');
@@ -42,7 +52,7 @@ const scaleSmallerButton = modalContainer.querySelector('.scale__control--smalle
 const slider = modalContainer.querySelector('.effect-level__slider');
 const sliderEffectList = modalContainer.querySelector('.effects__list');
 const sliderEffectLevel = modalContainer.querySelector('.effect-level__value');
-const effectsPreviews = modalContainer.querySelectorAll('.effects__preview');
+const effectsPreview = modalContainer.querySelectorAll('.effects__preview');
 const defaultEffect = modalContainer.querySelector('#effect-none');
 
 let currentEffect = Effect.DEFAULT;
@@ -50,10 +60,7 @@ let currentFilter = Filter.DEFAULT;
 let currentScale = SCALE_DEFAULT;
 
 noUiSlider.create(slider, {
-  range: {
-    min: 0,
-    max: 100
-  },
+  range: { min: 0, max: 100 },
   start: 100,
   step: 1,
   connect: 'lower',
@@ -63,94 +70,39 @@ noUiSlider.create(slider, {
   }
 });
 
+const setEffectsPreview = () => {
+  effectsPreview.forEach((effect) => {
+    effect.style.backgroundImage = `url('${imagePreview.src}')`;
+  });
+};
+
 const loadPreview = (imageInput) => {
   const imageFile = imageInput.files[0];
   const imageFileName = imageFile.name.toLowerCase();
   const matches = FILE_TYPES.some((item) => imageFileName.endsWith(item));
   if (matches) {
     imagePreview.src = URL.createObjectURL(imageFile);
-    effectsPreviews.forEach((el) => {
-      el.style.backgroundImage = `url('${imagePreview.src}')`;
-    });
+  } else {
+    imagePreview.src = PREVIEW_DEFAULT;
   }
-};
-
-const changeScalePreview = ({isSmaller = false, isBigger = false} = {}) => {
-  if (isSmaller === isBigger) {
-    return;
-  }
-  if (isSmaller && currentScale > MIN_SCALE) {
-    currentScale -= SCALE_STEP;
-  }
-  if (isBigger && currentScale < MAX_SCALE) {
-    currentScale += SCALE_STEP;
-  }
-  imagePreview.style.transform = `scale(${currentScale / 100})`;
-  imageScale.value = `${currentScale}%`;
-};
-
-const setFilterValue = ({unit = ''} = {}) => {
-  imagePreview.style.filter = `${currentFilter}(${sliderEffectLevel.value}${unit})`;
-};
-
-const changeFilterValue = () => {
-  switch (currentFilter) {
-    case Filter.GRAYSCALE:
-      setFilterValue();
-      break;
-    case Filter.SEPIA:
-      setFilterValue();
-      break;
-    case Filter.INVERT:
-      setFilterValue({unit: '%'});
-      break;
-    case Filter.BLUR:
-      setFilterValue({unit: 'px'});
-      break;
-    case Filter.BRIGHTNESS:
-      setFilterValue();
-      break;
-    default:
-      imagePreview.style.filter = Filter.DEFAULT;
-  }
+  setEffectsPreview();
 };
 
 const getCurrentOptions = () => {
   const [min, max, step] = Option[currentEffect];
-  return ({
-    range: {
-      min,
-      max,
-    },
-    start: max,
-    step
-  });
+  return ({ range: { min, max }, start: max, step });
 };
 
-const setCurrentEffect = () => {
-  switch(currentEffect) {
-    case Effect.CHROME:
-      currentFilter = Filter.GRAYSCALE;
-      slider.noUiSlider.updateOptions(getCurrentOptions());
-      break;
-    case Effect.SEPIA:
-      currentFilter = Filter.SEPIA;
-      slider.noUiSlider.updateOptions(getCurrentOptions());
-      break;
-    case Effect.MARVIN:
-      currentFilter = Filter.INVERT;
-      slider.noUiSlider.updateOptions(getCurrentOptions());
-      break;
-    case Effect.FOBOS:
-      currentFilter = Filter.BLUR;
-      slider.noUiSlider.updateOptions(getCurrentOptions());
-      break;
-    case Effect.HEAT:
-      currentFilter = Filter.BRIGHTNESS;
-      slider.noUiSlider.updateOptions(getCurrentOptions());
-      break;
-    default: currentFilter = Filter.DEFAULT;
-      slider.noUiSlider.updateOptions(getCurrentOptions());
+const setCurrentFilter = () => {
+  currentFilter = Dependence[currentEffect].style;
+  slider.noUiSlider.updateOptions(getCurrentOptions());
+};
+
+const setSliderShown = () => {
+  if (currentEffect === Effect.DEFAULT) {
+    imageEffectLevel.classList.add(ClassName.HIDDEN);
+  } else {
+    imageEffectLevel.classList.remove(ClassName.HIDDEN);
   }
 };
 
@@ -158,43 +110,50 @@ const resetImageStyle = () => {
   currentEffect = Effect.DEFAULT;
   currentFilter = Filter.DEFAULT;
   currentScale = SCALE_DEFAULT;
+  imageScale.value = `${SCALE_DEFAULT}%`;
   imagePreview.style.filter = Filter.DEFAULT;
   imagePreview.style.transform = Filter.DEFAULT;
-  imageScale.value = `${SCALE_DEFAULT}%`;
   sliderEffectLevel.value = null;
   defaultEffect.checked = true;
 };
 
+const changeScalePreview = () => {
+  imagePreview.style.transform = `scale(${currentScale / 100})`;
+  imageScale.value = `${currentScale}%`;
+};
+
+const onSmallerButtonClick = () => {
+  if (currentScale > MIN_SCALE) {
+    currentScale -= SCALE_STEP;
+    changeScalePreview();
+  }
+};
+
+const onBiggerButtonClick = () => {
+  if (currentScale < MAX_SCALE) {
+    currentScale += SCALE_STEP;
+    changeScalePreview();
+  }
+};
+
 slider.noUiSlider.on('update', () => {
   sliderEffectLevel.value = slider.noUiSlider.get();
-  changeFilterValue();
+  imagePreview.style.filter = `${currentFilter}(${sliderEffectLevel.value}${Dependence[currentEffect].unit})`;
 });
 
-scaleBiggerButton.addEventListener('click', () => {
-  changeScalePreview({isBigger: true});
-});
-
-scaleSmallerButton.addEventListener('click', () => {
-  changeScalePreview({isSmaller: true});
-});
+scaleSmallerButton.addEventListener('click', onSmallerButtonClick);
+scaleBiggerButton.addEventListener('click', onBiggerButtonClick);
 
 sliderEffectList.addEventListener('change', (evt) => {
   evt.preventDefault();
-  const effectItem = evt.target
-    .closest('.effects__item')
-    .querySelector('input[type="radio"]');
+  const effectItem = evt.target.closest('.effects__item').querySelector('input[type="radio"]');
 
   if (effectItem.checked) {
     currentEffect = effectItem.id;
   }
 
-  if (currentEffect === Effect.DEFAULT) {
-    toggleClass(imageEffectLevel, ClassName.HIDDEN, true);
-  } else {
-    toggleClass(imageEffectLevel, ClassName.HIDDEN, false);
-  }
-
-  setCurrentEffect();
+  setSliderShown();
+  setCurrentFilter();
 });
 
-export { Filter, imagePreview, imageEffectLevel, resetImageStyle, loadPreview };
+export { imagePreview, imageEffectLevel, resetImageStyle, loadPreview };
